11040b6893a66a65734b430e27b45fac7290f63b
artem1101gavrilov <artem1101gavrilov@gmail.com>
2018-07-12 11:51:16 +0300

    Add files via upload

---
+++ PracticeTasksC++/beginner_task.h
@@ -0,0 +1,6 @@
+#ifndef BEGINNER_TASK_H
+#define BEGINNER_TASK_H
+
+	void beginner_task_1();
+
+#endif
---
+++ PracticeTasksC++/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//Тут будут реализоваться задания с сайта:
+//http://cppstudio.com/cat/285/
+//"раздел предназначен для самостоятельного решения задач по С++"
+
+#include "beginner_task.h"
+
+void main(){
+
+	beginner_task_1();
+
+	std::system("pause");
+}
