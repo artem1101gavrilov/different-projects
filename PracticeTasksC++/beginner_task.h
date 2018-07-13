5570830e817e0970eefc142ca9027c0a1971194a
artem1101gavrilov <artem1101gavrilov@gmail.com>
2018-07-13 11:59:01 +0300

    Update beginner_task.h

--- PracticeTasksC++/beginner_task.h
+++ PracticeTasksC++/beginner_task.h
@@ -3,4 +3,17 @@
 
 	void beginner_task_1();
 
+	void beginner_task_2();
+
+	void beginner_task_3();
+
+	void beginner_task_4();
+
+	void beginner_task_5();
+
+	void beginner_task_6();
+
+	void beginner_task_7();
+
+	void beginner_task_8();
 #endif
