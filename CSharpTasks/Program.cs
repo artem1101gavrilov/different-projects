9959978c423b4e7c36e4d82ea6671167e70d9dde
artem1101gavrilov <artem1101gavrilov@gmail.com>
2019-01-31 13:41:18 +0300

    Update Program.cs

--- CSharpTasks/Program.cs
+++ CSharpTasks/Program.cs
@@ -1,10 +1,26 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace CSharpTasks
 {
+    class User
+    {
+        public static int k = 0;
+        static User()
+        {
+            Console.WriteLine("lalala");
+        }
+
+        void llll()
+        {
+
+        }
+    }
+
     class Program
     {
         public static void Task1()
@@ -195,11 +211,86 @@ namespace CSharpTasks
             Console.WriteLine(s);
             Console.WriteLine(elapsedTime);
         }
-        
+
+        public static void Task6()
+        {
+            User u1 = new User();
+            User u2 = new User();
+            Console.WriteLine(User.k);
+            Console.WriteLine(u2.ToString());
+
+            object x = null;
+            object y = x ?? 100;
+            Console.WriteLine(y);
+
+            int _sz = 65536;
+            int _sz2 = (int)(_sz * 0.9f);
+            Console.WriteLine(_sz2);
+
+            string a = "1";
+            switch (a)
+            {
+                case "1":
+                    Console.WriteLine("1");
+                    break;
+                default:
+                    Console.WriteLine("2");
+                    break;
+            }
+
+        }
+
+        public static void Task7()
+        {
+            List<Records> NameList = new List<Records>();
+            NameList.Add(new Records(){Name = "123", score = 13});
+
+            //Запись
+            FileStream fs = new FileStream("save.qwe", FileMode.Create);
+            BinaryFormatter bFormatter = new BinaryFormatter();
+            NameClassSave nameClassSave = new NameClassSave(NameList);
+            bFormatter.Serialize(fs, nameClassSave);
+            fs.Close();
+
+            //Считывание
+            FileStream fs2 = new FileStream("save.qwe", FileMode.Open);
+            BinaryFormatter bFormatter2 = new BinaryFormatter();
+            try
+            {
+                NameClassSave nameClassSave2 = (NameClassSave)bFormatter2.Deserialize(fs2);
+                Console.WriteLine(nameClassSave2.NameList[0].Name);
+            }
+            catch(System.Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                fs2.Close();
+            }
+        }
+
         static void Main(string[] args)
         {
-            Task3();
+            Task7();
             Console.ReadKey(); 
+        }
+    }
+
+    [Serializable]
+    public struct Records
+    {
+        public string Name;
+        public int score;
+    }
+
+    [Serializable]
+    public class NameClassSave
+    {
+        public List<Records> NameList;
+        public NameClassSave(List<Records> NewNameList)
+        {
+            NameList = NewNameList;
         }
     }
 }
