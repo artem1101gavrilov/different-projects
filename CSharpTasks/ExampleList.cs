bf9be37975d1eff3cd4db430901eb3e542bd2736
artem1101gavrilov <artem1101gavrilov@gmail.com>
2019-02-20 17:36:07 +0300

    Пример работы с листом.

---
+++ CSharpTasks/ExampleList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace CSharpTasks
+{
+    class GameObject
+    {
+        public string name;
+        public float x;
+    }
+
+    class Program
+    {
+
+        public static void ExampleList()
+        {
+            List<GameObject> listGO = new List<GameObject>();
+            listGO.Add(new GameObject());
+            listGO[0].name = "GO1";
+            Console.WriteLine(listGO[0].name);
+        }
+
+        static void Main(string[] args)
+        {
+            ExampleList();
+            Console.ReadKey(); 
+        }
+    }
+}
