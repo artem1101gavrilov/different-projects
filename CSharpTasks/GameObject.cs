4dcb8dcf7d53a43f1a1e60b8d08b49018ef4a679
artem1101gavrilov <artem1101gavrilov@gmail.com>
2019-03-20 11:48:12 +0300

    Update GameObject.cs

--- CSharpTasks/GameObject.cs
+++ CSharpTasks/GameObject.cs
@@ -9,9 +9,9 @@ namespace CSharpTasks
 {
     struct Vector3
     {
-        public int x { get; protected set; }
-        public int y { get; private set; }
-        public int z { get; private set; }
+        public int x;
+        public int y;
+        public int z;
         public Vector3(int x1, int y1, int z1)
         {
             x = x1;
@@ -22,7 +22,17 @@ namespace CSharpTasks
 
     class Transform
     {
-        public Vector3 position;
+        Vector3 vector3;
+        public Vector3 position 
+        {
+            get{
+                return vector3;
+            }
+            set
+            {
+                vector3 = value;
+            }
+        }
     }
 
     class GameObject
@@ -37,10 +47,11 @@ namespace CSharpTasks
         {
             GameObject a = new GameObject();
             //a.transform.position.x = 2; //ERROR
+            a.transform.position = new Vector3(1, 2, 3);
             Console.Write(a.transform.position.x);
 
             var s = new Vector3();
-            //s.x += 1; //ERROR
+            s.x += 1;
 
             Console.ReadKey(); 
         }
