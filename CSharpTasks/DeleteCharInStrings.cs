311503b84fbcfe8f9f4279e3bc021e0bf8e42e53
artem1101gavrilov <artem1101gavrilov@gmail.com>
2019-09-19 15:42:01 +0300

    Удаление всех символов в строке.

---
+++ CSharpTasks/DeleteCharInStrings.cs
@@ -0,0 +1,34 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+class Solution
+{
+
+    static void Main()
+    {
+        string obs = "!@#$%^&*()";
+        string lalala = "sd(f)sf#$%g*d";
+
+        foreach (char c in obs)
+        {
+            lalala = lalala.Replace(c.ToString() , string.Empty);
+        }
+
+        Console.WriteLine(lalala);
+        Console.ReadLine();
+    }
+
+}
