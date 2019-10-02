f456030f2ec4d7b0303c6f60a6a157c0092467cd
artem1101gavrilov <artem1101gavrilov@gmail.com>
2019-10-02 20:43:20 +0300

    Работа со структурами

---
+++ CSharpTasks/Program0210.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MC m = new MC();
+            var d = m.Parameters[0];
+            d.T = 5;
+            m.Parameters[0] = d;
+
+            foreach (var a in m.Parameters)
+            {
+                Console.WriteLine(a.T);
+            }
+            Console.ReadLine();
+        }
+    }
+
+    public struct MCP
+    {
+        public float T;
+    }
+
+    public class MC
+    {
+        List<MCP> parameters = new List<MCP>();
+
+        public MC()
+        {
+            parameters.Add(new MCP(){ T = 1 });
+            parameters.Add(new MCP(){ T = 2 });
+            parameters.Add(new MCP(){ T = 3 });
+        }
+
+        public List<MCP> Parameters
+        {
+            get { return parameters; }
+        }
+    }
+}
