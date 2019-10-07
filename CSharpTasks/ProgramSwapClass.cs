5b2f7e8f9638aa8d40232e11819522a0256d94d6
artem1101gavrilov <artem1101gavrilov@gmail.com>
2019-10-07 12:24:46 +0300

    Swap классов

---
+++ CSharpTasks/ProgramSwapClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Numerics;
+
+class Solution
+{
+    static void Main()
+    {
+        ExampleClass e1 = new ExampleClass(1);
+        ExampleClass e2 = new ExampleClass(2);
+        Console.WriteLine(e1);
+        Console.WriteLine(e2);
+        e1.Swap(e1, e2);
+        Console.WriteLine(e1);
+        Console.WriteLine(e2);
+        e1.SwapRef(ref e1, ref e2);
+        Console.WriteLine(e1);
+        Console.WriteLine(e2);
+        Console.ReadLine();
+    }
+}
+
+class ExampleClass
+{
+    private int id;
+    public ExampleClass(int id)
+    {
+        this.id = id;
+    }
+    public override string ToString()
+    {
+        return "ExampleClass" + id;
+    }
+    //не работает смена
+    public void Swap(ExampleClass ex1, ExampleClass ex2)
+    {
+        ExampleClass buf = ex1;
+        ex1 = ex2;
+        ex2 = ex1;
+    }
+    //работает смена
+    public void SwapRef(ref ExampleClass ex1, ref ExampleClass ex2)
+    {
+        ExampleClass buf = ex1;
+        ex1 = ex2;
+        ex2 = buf;
+    }
+}
