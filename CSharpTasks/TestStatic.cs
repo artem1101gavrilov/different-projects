077fcf9cfaedb157253bf61b45ec131cffda3cd9
artem1101gavrilov <artem1101gavrilov@gmail.com>
2019-10-10 14:03:02 +0300

    Add files via upload

---
+++ CSharpTasks/TestStatic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+class Solution
+{
+    static void Main()
+    {
+        ExampleClass Example = new ExampleClass();
+        Console.Write(Example.e);
+        Console.ReadLine();
+    }
+}
+
+class ExampleClass
+{
+    static int i;
+    public int e;
+    public ExampleClass()
+    {
+        e = i;
+    }
+    static ExampleClass()
+    {
+        i = 1101;
+    }
+}
