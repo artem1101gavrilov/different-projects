4e615344677d16c52433810101973057e2570dbf
artem1101gavrilov <artem1101gavrilov@gmail.com>
2019-10-18 10:55:40 +0300

    Update AddNewElementInList.cs

--- CSharpTasks/AddNewElementInList.cs
+++ CSharpTasks/AddNewElementInList.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -15,7 +15,7 @@ class Solution
         arr.Add(5);
 
         //Разбиваю лист на массивы 123 и 45
-        int[] arr2 = new int[3];
+        /*int[] arr2 = new int[3];
         int[] arr3 = new int[2];
         arr.CopyTo(0, arr2, 0, 3);
         arr.CopyTo(3, arr3, 0, 2);
@@ -24,13 +24,38 @@ class Solution
         List<int> arr4 = new List<int>();
         arr4.AddRange(arr2);
         arr4.Add(9);
-        arr4.AddRange(arr3);
+        arr4.AddRange(arr3);*/
+
+        arr.AddNewElement(9, 3);
 
         //Вывод данных на экран
-        foreach (var i in arr4)
+        foreach (var i in arr)
         {
             Console.Write(i);
         }
+
         Console.ReadLine();
+    }
+}
+
+public static class AddNewElementList
+{
+    public static void AddNewElement(this List<int> list, int newElement, int index)
+    {
+        //Если размер меньше, то выходим
+        if(index > list.Count) return;
+        var lastIndex = list.Count - index;
+
+        //Разбиваю лист на массивы
+        int[] arr1 = new int[index];
+        int[] arr2 = new int[lastIndex];
+        list.CopyTo(0, arr1, 0, index);
+        list.CopyTo(index, arr2, 0, lastIndex);
+
+        //Объединение двух массивов и нового числа
+        list.Clear();
+        list.AddRange(arr1);
+        list.Add(newElement);
+        list.AddRange(arr2);
     }
 }
