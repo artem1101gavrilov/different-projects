39fbd1c7c4f3510cdce1aa1003df9dc8560234ec
artem1101gavrilov <artem1101gavrilov@gmail.com>
2019-11-05 12:25:00 +0300

    Add files via upload

---
+++ CSharpTasks/ProgramRemoveList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Solution
+{
+    static void Main()
+    {
+        List<int> a = new List<int>() {1, 0, 2, 0, 3, 0, 4, 0, 0 };
+        for (int i = 0; i < a.Count; ++i)
+        {
+            if (a[i] == 0) a.Remove(a[i]);
+        }
+        for (int i = 0; i < a.Count; ++i)
+        {
+            Console.Write(a[i]); // 1 2 3 4 0
+        }
+
+        List<int> b = new List<int>() { 1, 0, 2, 0, 3, 0, 4, 0, 0 };
+        for (int i = 0; i < b.Count; ++i)
+        {
+            if (b[i] == 0)
+            {
+                b.Remove(b[i]);
+                --i;
+            }
+        }
+        for (int i = 0; i < b.Count; ++i)
+        {
+            Console.Write(b[i]); // 1 2 3 4
+        }
+        
+        Console.ReadLine();
+    }
+}
