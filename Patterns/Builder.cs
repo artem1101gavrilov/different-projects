418b315d53ed16f2918f4719c55ffa89821114d9
Гаврилов Артем <dede13@yandex.ru>
2021-07-11 11:50:24 +0300

    Пример паттернов с metanit и с refactoring.guru.

---
+++ Patterns/Builder.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+
+public sealed class Program 
+{
+    public interface IBuilder
+    {
+        void BuildPartA();
+        void BuildPartB();
+        void BuildPartC();
+    }
+
+    public class ConcreteBuilder : IBuilder
+    {
+        private Product product = new Product();
+
+        public ConcreteBuilder()
+        {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            product = new Product();
+        }
+
+        public void BuildPartA()
+        {
+            product.Add("PartA1");
+        }
+
+        public void BuildPartB()
+        {
+            product.Add("PartB1");
+        }
+
+        public void BuildPartC()
+        {
+            product.Add("PartC1");
+        }
+
+        public Product GetProduct()
+        {
+            var result = product;
+            Reset();
+            return result;
+        }
+    }
+
+    public class BreadBuilder : IBuilder
+    {
+        private Bread bread = new Bread();
+
+        public void Reset()
+        {
+            bread = new Bread();
+        }
+
+        public void BuildPartA()
+        {
+            bread.Add("Мука");
+        }
+
+        public void BuildPartB()
+        {
+            bread.Add("Соль");
+        }
+
+        public void BuildPartC()
+        {
+            bread.Add("Добавки");
+        }
+
+        public Product GetProduct()
+        {
+            var result = bread;
+            Reset();
+            return result;
+        }
+    }
+
+    public class Product
+    {
+        protected List<object> parts = new List<object>();
+
+        public void Add(string part)
+        {
+            parts.Add(part);
+        }
+
+        public virtual string ListParts()
+        {
+            string str = string.Join(", ", parts);
+            return "Product parts: " + str + "\n";
+        }
+    }
+
+    public class Bread : Product
+    {
+        public override string ListParts()
+        {
+            string str = string.Join(", ", parts);
+            return "Bread parts: " + str + "\n";
+        }
+    }
+
+    public class Director
+    {
+        private IBuilder builder;
+
+        public IBuilder Builder
+        {
+            set { builder = value; }
+        }
+
+        public void BuildMinimalViableProduct()
+        {
+            builder.BuildPartA();
+        }
+
+        public void BuildFullFeaturedProduct()
+        {
+            builder.BuildPartA();
+            builder.BuildPartB();
+            builder.BuildPartC();
+        }
+    }
+
+    public static void Main() 
+    {
+        var director = new Director();
+        var builder = new ConcreteBuilder();
+        director.Builder = builder;
+
+        Console.WriteLine("Standard basic product:");
+        director.BuildMinimalViableProduct();
+        Console.WriteLine(builder.GetProduct().ListParts());
+
+        Console.WriteLine("Standard full featured product:");
+        director.BuildFullFeaturedProduct();
+        Console.WriteLine(builder.GetProduct().ListParts());
+
+        var breadBuilder = new BreadBuilder();
+        director.Builder = breadBuilder;
+        Console.WriteLine("Standard basic product:");
+        director.BuildMinimalViableProduct();
+        Console.WriteLine(breadBuilder.GetProduct().ListParts());
+
+        Console.WriteLine("Standard full featured product:");
+        director.BuildFullFeaturedProduct();
+        Console.WriteLine(breadBuilder.GetProduct().ListParts());
+
+        Console.WriteLine("Custom product:");
+        builder.BuildPartA();
+        builder.BuildPartC();
+        Console.Write(builder.GetProduct().ListParts());
+        Console.ReadKey();
+    }
+}
+
---
+++ Patterns/Proxy.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+
+public sealed class Program 
+{
+    public interface ISubject
+    {
+        void Request();
+    }
+
+    class RealSubject : ISubject
+    {
+        public void Request()
+        {
+            Console.WriteLine("RealSubject: Handling Request.");
+        }
+    }
+
+    class Proxy : ISubject
+    {
+        private RealSubject realSubject = new RealSubject();
+
+        public Proxy(RealSubject realSubject)
+        {
+            this.realSubject = realSubject;
+        }
+
+        public void Request()
+        {
+            if (CheckAccess())
+            {
+                realSubject.Request();
+                LogAccess();
+            }
+        }
+
+        public bool CheckAccess()
+        {
+            Console.WriteLine("Proxy: Checking access prior to firing a real request.");
+            return true;
+        }
+
+        public void LogAccess()
+        {
+            Console.WriteLine("Proxy: Logging the time of request.");
+        }
+    }
+
+    public class Client
+    {
+        public void ClientCode(ISubject subject)
+        {
+            subject.Request();
+        }
+    }
+
+    public static void Main() 
+    {
+        Client client = new Client();
+
+        Console.WriteLine("Client: Executing the client code with a real subject:");
+        RealSubject realSubject = new RealSubject();
+        client.ClientCode(realSubject);
+
+        Console.WriteLine();
+
+        Console.WriteLine("Client: Executing the same client code with a proxy:");
+        Proxy proxy = new Proxy(realSubject);
+        client.ClientCode(proxy);
+        Console.ReadKey();
+    }
+}
+
